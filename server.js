ca632fc6d8345c1aa0ab0d1a59dba3324bbf3198
mcs77-cyber <mcs77@aluno.ifal.edu.br>
2026-06-11 11:46:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 // TODO: Implemente as rotas:
-// GET /       → 200, corpo contém "Home"
-// GET /sobre  → 200, corpo contém "Sobre"
-// GET /contato→ 200, corpo contém "Contato"
+GET /       → 200
+GET /sobre  → 200
+GET /contato → 200
+GET /rota-inexistente → 404
 // Rotas não mapeadas → 404 automático do Express
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
