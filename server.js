a7be132175632804b0895c33bb68e1141b5ce0a8
MAX CLAY DA SILVA <2024332799@MACEIO.IFAL>
2026-08-12 11:26:30 -0300

    hi

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 
 // TODO: Implemente as rotas:
-GET /       → 200
-GET /sobre  → 200
-GET /contato → 200
-GET /rota-inexistente → 404
+// GET /       → 200
+// GET /sobre  → 200
+// GET /contato → 200
+// GET /rota-inexistente → 404
 // Rotas não mapeadas → 404 automático do Express
+
+app.get('/',(req,res) => 
+    app.res("Home")
+)
+
+app.get('/sobre',(req,res) => 
+    app.res("Sobre")
+)
+
+
+app.get('/contato',(req,res) => 
+    app.res("Contato")
+)
+app.use((req, res) => {
+  res.status(404).send('Oops! Essa paigna é inexistente');
+});
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
